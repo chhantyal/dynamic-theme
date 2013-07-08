b2b482ddaf689c5e9f947d78a8cd948cf6be35f4
Nar Kumar <neokya@gmail.com>
2013-07-09 01:02:19 +0530

    initial commit

---
+++ theme/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
+from django.utils.text import slugify
+from django.db.models import signals
+from django.template.loader import render_to_string
+from django.core.files.base import ContentFile
+
+from socialschools.settings.s3storage import PublicMediaS3BotoStorage
+
+from tasks import compile_less
+
+class Theme(models.Model):
+    def get_upload_path(self, filename):
+        """
+        Gets the upload path for files
+        """
+        filename = filename.split('.')
+        extension = filename.pop()
+        name = ''.join(filename)
+        return "themes/%s/%s.%s" % (self.organization.pk, slugify(unicode(name)), extension)
+
+    name = models.CharField(max_length=250)
+    organization = models.OneToOneField('profiles.BaseOrganizationalUnit')
+    theme_color = models.CharField(max_length=7)
+    background_image = models.ImageField(storage=PublicMediaS3BotoStorage(), upload_to=get_upload_path, blank=True)
+    less_file = models.FileField(storage=PublicMediaS3BotoStorage(), upload_to=get_upload_path, blank=True)
+    compiled_css_file = models.FileField(storage=PublicMediaS3BotoStorage(), upload_to=get_upload_path, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_less_content(self):
+        less_content = render_to_string('theme/theme.less', {'instance' : self})
+        self.less_file.save('theme.less', ContentFile(less_content), save=False)
+        return less_content
+
+@receiver(signals.post_save, sender=Theme)
+def generate_less_file(sender, instance, *args, **kwargs):
+    if not getattr(instance, '_do_not_compile', None):
+        compile_less.delay(instance, instance.get_less_content())
